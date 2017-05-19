0b395584a992680a2d9da9b03c963f7c34bc2607
Richard Igbiriki <imorobebh@gmail.com>
2017-05-19 00:13:25 -0400

    Merge Conflict Resolution

--- src/components/advising.js
+++ src/components/advising.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
-export default class Advising extends Component {
+export class Advising extends Component {
   render () {
     return (
       <div id="page-wrapper">
